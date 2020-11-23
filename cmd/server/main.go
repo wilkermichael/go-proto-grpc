311ef622ded278ef85a2f1f38b3af3b127027e87
Michael Wilkerson <michael.wilkerson@ca.abb.com>
2020-11-23 09:50:07 -0800

    daily commit

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,37 +3,30 @@ package main
 import (
 	"bytes"
 	"encoding/binary"
-	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"net"
 	"os"
-	"strings"
 
 	"github.com/golang/protobuf/proto"
 	"github.com/wilkermichael/go-proto-grpc/internal/protobuf"
+	"golang.org/x/net/context"
+	grpc "google.golang.org/grpc"
 )
 
 func main() {
-	flag.Parse()
-	if flag.NArg() < 1 {
-		fmt.Fprintln(os.Stderr, "missing subcommand: list or add")
-		os.Exit(1)
-	}
-
-	var err error
-	switch cmd := flag.Arg(0); cmd {
-	case "list":
-		err = list()
-	case "add":
-		err = add(strings.Join(flag.Args()[1:], " "))
-	default:
-		err = fmt.Errorf("unknown subcommand %s", cmd)
-	}
+	srv := grpc.NewServer()
+	var tasks taskServer
+	protobuf.RegisterTasksServer(srv, tasks)
+	l, err := net.Listen("tcp", ":8888")
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		log.Fatalf("could not listen to :8888: %v", err)
 	}
+	log.Fatal(srv.Serve(l))
 }
+
+type taskServer struct{}
 
 type length int64
 
@@ -44,66 +37,61 @@ const (
 
 var endianness = binary.LittleEndian
 
-func add(text string) error {
+func (taskServer) Add(ctx context.Context, text *protobuf.Text) (*protobuf.Task, error) {
 	task := &protobuf.Task{
-		Text: text,
+		Text: text.Text,
 		Done: false,
 	}
 
 	b, err := proto.Marshal(task)
 	if err != nil {
-		return fmt.Errorf("could not encode task: %v", err)
+		return nil, fmt.Errorf("could not encode task: %v", err)
 	}
 
 	f, err := os.OpenFile(dbPath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
-		return fmt.Errorf("could not open %s: %v", dbPath, err)
+		return nil, fmt.Errorf("could not open %s: %v", dbPath, err)
 	}
 
 	if err := binary.Write(f, endianness, length(len(b))); err != nil {
-		return fmt.Errorf("could not encode length of message: %v", err)
+		return nil, fmt.Errorf("could not encode length of message: %v", err)
 	}
 	_, err = f.Write(b)
 	if err != nil {
-		return fmt.Errorf("could not write task to file: %v", err)
+		return nil, fmt.Errorf("could not write task to file: %v", err)
 	}
 
 	if err := f.Close(); err != nil {
-		return fmt.Errorf("could not close file %s: %v", dbPath, err)
+		return nil, fmt.Errorf("could not close file %s: %v", dbPath, err)
 	}
-	return nil
+	return task, nil
 }
 
-func list() error {
+func (taskServer) List(ctx context.Context, void *protobuf.Void) (*protobuf.TaskList, error) {
 	b, err := ioutil.ReadFile(dbPath)
 	if err != nil {
-		return fmt.Errorf("could not read %s: %v", dbPath, err)
+		return nil, fmt.Errorf("could not read %s: %v", dbPath, err)
 	}
 
+	var tasks protobuf.TaskList
 	for {
 		if len(b) == 0 {
-			return nil
+			return &tasks, nil
 		} else if len(b) < sizeOfLength {
-			return fmt.Errorf("remaining odd %d bytes, what to do?", len(b))
+			return nil, fmt.Errorf("remaining odd %d bytes, what to do?", len(b))
 		}
 
 		var l length
 		if err := binary.Read(bytes.NewReader(b[:sizeOfLength]), endianness, &l); err != nil {
-			return fmt.Errorf("could not decode message length: %v", err)
+			return nil, fmt.Errorf("could not decode message length: %v", err)
 		}
 		b = b[sizeOfLength:]
 
 		var task protobuf.Task
 		if err := proto.Unmarshal(b[:l], &task); err != nil {
-			return fmt.Errorf("could not read task: %v", err)
+			return nil, fmt.Errorf("could not read task: %v", err)
 		}
 		b = b[l:]
-
-		if task.Done {
-			fmt.Printf("👍")
-		} else {
-			fmt.Printf("😱")
-		}
-		fmt.Printf(" %s\n", task.Text)
+		tasks.Tasks = append(tasks.Tasks, &task)
 	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module github.com/wilkermichael/go-proto-grpc
 go 1.15
 
 require (
+	github.com/campoy/justforfunc v0.0.0-20181017025055-cba17b08c40d
+	github.com/gogo/protobuf v1.1.1
 	github.com/golang/protobuf v1.4.3
-	google.golang.org/protobuf v1.23.0
+	golang.org/x/net v0.0.0-20190311183353-d8887717615a
+	google.golang.org/grpc v1.33.2
+	google.golang.org/protobuf v1.25.0
 )
